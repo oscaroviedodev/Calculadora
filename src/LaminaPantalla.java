9c2208bf205c11a67ef256fa2b20a9e7831a0466
Oscar Oviedo <oscaroviedodev@outlook.com>
2026-01-02 16:06:38 -0500

    feat: crear pantalla de la calculadora

--- src/LaminaPantalla.java
+++ src/LaminaPantalla.java
@@ -1,5 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class LaminaPantalla extends JPanel{
     
+    JTextField pantalla = new JTextField("0");
+        
+    public LaminaPantalla() {
+        this.add(pantalla);        
+        pantalla.setHorizontalAlignment(JTextField.RIGHT); 
+        pantalla.setPreferredSize(new Dimension(285, 30));
+        pantalla.setFont(new Font("",Font.BOLD,20));
+        pantalla.setEnabled(false);
+
+    }
 }
