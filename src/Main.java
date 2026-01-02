083182c414c5e31566f33d64eca86892fe5e6f5d
Oscar Oviedo <oscaroviedodev@outlook.com>
2026-01-02 12:05:40 -0500

    feat: crear marco de la calculadora

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Marco miMarco = new Marco();
+    }
+}
---
+++ src/Marco.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Marco extends JFrame{
+    
+    public Marco() {
+        this.setLocation(1200, 200);
+        this.setSize(300, 450);
+        this.setTitle("Calculadora");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+        //falta crear coomit
+    }
+}
